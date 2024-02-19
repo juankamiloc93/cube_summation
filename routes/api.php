6524741cb1a0fae435c9407f23d017b6dac5a9d2
juankamilo <juankamiloc93@gmail.com>
2024-02-18 23:19:12 -0500

    feat: create route cubes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\CubeSummation;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(CubeSummation::class)->group(function () {
+    Route::post('cubes', 'executeCubeSummation');  
+});  
+
